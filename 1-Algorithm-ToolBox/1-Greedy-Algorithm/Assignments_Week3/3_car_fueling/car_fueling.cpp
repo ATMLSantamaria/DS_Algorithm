429cc4f7136ad73511dac674a87591689b76441b
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2022-01-23 19:34:01 +0100

    Minimize Car Fueling

--- 1-Algorithm-ToolBox/1-Greedy-Algorithm/Assignments_Week3/3_car_fueling/car_fueling.cpp
+++ 1-Algorithm-ToolBox/1-Greedy-Algorithm/Assignments_Week3/3_car_fueling/car_fueling.cpp
@@ -10,20 +10,25 @@ int compute_min_refills(int dist, int tank, vector<int> & stops) {
     int num_of_refills=0;
     int i=0; //current_refill
     int n = stops.size();
+    stops[n-1]=dist;
     // write your code here
-    while(i < n){
+    int nn=n-2;
+    while(i <= nn /*n-2*/){
 
         //Extraigo mi posicion de repostaje
         int last_refill = i;
         //GREEDY CHOICE THAT I HAVE PROVE IS A SAFE MOVE
         //Avanzo todo lo posible sin repostar mientras la siguiente parada este disponible. 
-        while(stops[i+1] - stops[last_refill] < tank && i<n){
+        while((stops[i+1] - stops[last_refill]) <= tank && i<=nn /*n-2*/ ){
+            //int tmp_next =stops[i+1];
+            //int tmp_last =stops[last_refill];
             i++;
         }
         if(i==last_refill) return -1;
         //Aqui repostas, asi que aumentas el numero de repostajes
-        if (i <= n){
+        if (i <= nn){
             num_of_refills++;
+            //std::cout << " He parado en " << i << ' '<< stops[i] <<std::endl;
         }
     }
 
@@ -40,8 +45,10 @@ int main() {
     int n = 0;
     cin >> n;
 
-    vector<int> stops(n);
-    for (size_t i = 0; i < n; ++i)
+    vector<int> stops(n+2);
+    
+    stops[n+1]=d;
+    for (size_t i = 1; i <= n; ++i)
         cin >> stops.at(i);
 
     cout << compute_min_refills(d, m, stops) << "\n";
