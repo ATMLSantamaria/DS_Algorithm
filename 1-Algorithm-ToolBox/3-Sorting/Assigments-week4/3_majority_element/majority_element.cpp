827138f4f9931ee0593bc2755884c21634935576
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2022-02-03 22:17:52 +0100

    Majority Element In array Divide And Conque
    Pure divide and conquer algorithm to find the
    majority element, without the need of sorting.
    Running time O(nlog(n))

--- 1-Algorithm-ToolBox/3-Sorting/Assigments-week4/3_majority_element/majority_element.cpp
+++ 1-Algorithm-ToolBox/3-Sorting/Assigments-week4/3_majority_element/majority_element.cpp
@@ -4,42 +4,48 @@
 
 using std::vector;
 
-int get_majority_element2(vector<int> &a, int left, int right) {
-  //right is a.size, so the max value is righ-1
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //write your code here
-  int mid = left+(right-left)/2;
-  int x = get_majority_element2(a,left,mid);
-  int y = get_majority_element2(a,mid,right);
-  if(x==y){
-    return x;
-  } else if(x!=-1 && y!=-1) {
-    int count_x=0,count_y=0;
-    for (int i=left;i<right;++i){
-      if (a[i]==x){
-        count_x++;
-      } else if (a[i]==y){
-        count_y++;
-      }
-      
-      
-    }
-    if((count_y > (right-left)/2  || count_x > (right-left)/2 )){
-        return count_y >= count_x ? y : x;
-    }
-  } 
-  return -1;
-}
-
 int get_majority_element(vector<int> &a, int left, int right) {
   //right is a.size, so the max value is righ-1
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
   //write your code here
+  int mid = left+(right-left)/2;
+  int x = get_majority_element(a,left,mid);
+  int y = get_majority_element(a,mid,right);
+  //Si combinacion es igual. Hay mayoritario
+  if(x==y){
+    return x;
+  } else {
+    int count_x=0,count_y=0;
+    for (int i=left;i<right;++i){
+      if (a[i]==y){
+        count_y++;
+      }
+      if (a[i]==x){
+        count_x++;
+      }
+    }
+    if (count_y > (right - left)/2){
+      return y;
+    } else if (count_x > (right - left)/2){
+      return x;
+    } else {
+      return -1;
+    }
+  }
+  return -1;
+}
+
+
+int get_majority_element_for_test(vector<int> &a, int left, int right) {
+  //right is a.size, so the max value is righ-1
+  //if (left == right) return -1;
+  //if (left + 1 == right) return a[left];
+  //write your code here
   std::sort(a.begin(),a.end());
   int count=1;
   int initialValue=a[0];
+  if (a.size()==1){return initialValue;}
   for (int i=1;i<a.size();++i){
     if(a[i]==initialValue){
       count++;
@@ -67,8 +73,8 @@ int main() {
   std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
 }
 /*
-5
-2 3 9 2 2
+6
+2 3 9 2 2 1
 
 4
 1 2 3 4
