0544a9c1d148b51aa802fe5a385e82a222e4ac38
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2022-02-04 23:27:51 +0100

    Improved version of QuickSort
    Partition in 3 groups
    Randomized pivot

--- 1-Algorithm-ToolBox/3-Sorting/Assigments-week4/4_improving_quicksort/sorting.cpp
+++ 1-Algorithm-ToolBox/3-Sorting/Assigments-week4/4_improving_quicksort/sorting.cpp
@@ -17,38 +17,6 @@ int partition2(vector<int> &a, int l, int r) {
   swap(a[l], a[j]);
   return j;
 }
-std::pair<int,int> partition3(std::vector<int> &A,int l ,int r){
-  //Pivot
-  int x = A[l];
-  int m1=l;
-  int m2=l;
-  for (int i=l+1;i<=r;++i){
-    if(A[i]<=x){
-      m2++;
-      std::swap(A[i],A[m2]);
-    }
-  }
-  std::swap(A[l],A[m2]);
-  for (int i=0;i<A.size();++i){
-    if(A[i]==x){
-      m1=i;
-      break;
-    }
-  }
-  return std::pair<int,int>(m1,m2);
-}
-void randomized_quick_sort3(vector<int> &a, int l, int r) {
-  if (l >= r) {
-    return;
-  }
-
-  int k = l + rand() % (r - l + 1);
-  swap(a[l], a[k]);
-  std::pair<int,int> m = partition3(a, l, r);
-
-  randomized_quick_sort3(a, l, m.first - 1);
-  randomized_quick_sort3(a, m.second + 1, r);
-}
 void randomized_quick_sort(vector<int> &a, int l, int r) {
   if (l >= r) {
     return;
@@ -61,6 +29,36 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
   randomized_quick_sort(a, l, m - 1);
   randomized_quick_sort(a, m + 1, r);
 }
+std::pair<int,int> partition_in_3(vector<int> &a, int l, int r) {
+  int x = a[l];
+  int j = l;
+  for (int i = l + 1; i <= r; i++) {
+    if (a[i] <= x) {
+      j++;
+      swap(a[i], a[j]);
+    }
+  }
+  swap(a[l], a[j]);
+  int m1=j;
+  for (int i=j;i==x;--i){
+    m1=i;
+  }
+
+  return std::pair<int,int>(m1,j);
+}
+void randomized_quick_sort_version3(vector<int> &A, int l, int r) {
+  if (l >= r) {
+    return;
+  }
+
+  int k = l + rand() % (r - l + 1);
+  swap(A[l], A[k]);
+  std::pair<int,int> m = partition_in_3(A, l, r);
+
+  randomized_quick_sort_version3(A, l, m.first - 1);
+  randomized_quick_sort_version3(A, m.second + 1, r);
+}
+
 
 int main() {
   int n;
@@ -69,21 +67,22 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  //vector<int> aa{a};
+  vector<int> aa{a};
+  std::cout << "Version Normal" << std::endl;
 
-  randomized_quick_sort3(a, 0, a.size() - 1);
+  randomized_quick_sort(a, 0, a.size() - 1);
   for (size_t i = 0; i < a.size(); ++i) {
     std::cout << a[i] << ' ';
   }
 
+  std::cout << std::endl;
+  std::cout << "Version 3" << std::endl;
 
-  //std::cout << std::endl;
-
-  // randomized_quick_sort3(aa,0,aa.size()-1);
-  //   for (size_t i = 0; i < aa.size(); ++i) {
-  //   std::cout << aa[i] << ' ';
-  // }
-  // std::cout << std::endl;
+  randomized_quick_sort_version3(aa,0,aa.size()-1);
+    for (size_t i = 0; i < aa.size(); ++i) {
+    std::cout << aa[i] << ' ';
+  }
+  std::cout << std::endl;
 
 }
 /*
